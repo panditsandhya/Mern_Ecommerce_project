e44877e589aa3136bfe113d5ee208e9efbab9048
Sandhya110 <starsandhya369@gmail.com>
2024-06-12 20:24:48 +0530

    ecommerce project

---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//Register || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+
+export default router;
